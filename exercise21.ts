3d6b9538dc40f19c03bef6f6ce6d1447faafde9a
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-25 18:37:51 +0200

    exercise21

--- exercise21.ts
+++ exercise21.ts
@@ -15,7 +15,7 @@ interface Country {
 
 type PartialCountry = Country;
 
-const countryA: PartialCountry = {
+const countryA: Partial <PartialCountry> = {
     code: "CN",
     population: 1_412_600_000,
 };
@@ -26,11 +26,12 @@ const countryA: PartialCountry = {
 
 type CompleteCountry = Country;
 
-const countryB: CompleteCountry = {
+const countryB: Required <CompleteCountry> = {
     name: "Greece",
     code: "GR",
     languages: ["Greek"],
     population: 10_678_632,
+    currency: "Greece"
 };
 
 // Change the `ReadonlyCountry` type to use the `Readonly` utility type.
@@ -39,14 +40,14 @@ const countryB: CompleteCountry = {
 
 type ReadonlyCountry = Country;
 
-const countryC: ReadonlyCountry = {
+const countryC: Readonly<ReadonlyCountry> = {
     name: "Italy",
     code: "IT",
     languages: ["Italian"],
     population: 60_317_116,
 };
 
-countryC.population = 60_317_117;
+
 
 console.log(countryC);
 
@@ -56,7 +57,7 @@ console.log(countryC);
 
 type CountryWithPopulation = Country;
 
-const countryD: CountryWithPopulation = {
+const countryD: Pick<CountryWithPopulation, ("name" | "code" | "population")> = {
     name: "New Zealand",
     code: "NZ",
     population: 5_135_300,
@@ -67,7 +68,7 @@ const countryD: CountryWithPopulation = {
 
 type CountryWithoutPopulation = Country;
 
-const countryE: CountryWithoutPopulation = {
+const countryE: Omit<CountryWithoutPopulation,("population")> = {
     name: "Thailand",
     code: "TH",
     languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
