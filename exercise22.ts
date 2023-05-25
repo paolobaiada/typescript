81418e60283c0f15ea802aa7b520a88deaa9f47d
PaoloBaiada <paolosettemaggio@gmail.com>
2023-05-25 18:52:54 +0200

    exercise22

--- exercise22.ts
+++ exercise22.ts
@@ -4,7 +4,7 @@
 
 // Fix this use of the `Record` utility type so it constructs a valid type.
 
-const currency: Record<> = {
+const currency: Record<string,string> = {
     name: "United States dollar",
     code: "USD",
     symbol: "$",
@@ -20,8 +20,8 @@ interface Currency {
     symbol: string;
 }
 
-const currencies = {
-    france: {
+const currencies : Record<string,Currency> = {
+    france:  {
         name: "Euro",
         code: "EUR",
         symbol: "€",
